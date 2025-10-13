544b39bf23be1468f931145e4f1bc4ed32c9d58e
Martina Tarantino <martinatarantino02@gmail.com>
2025-10-13 15:28:34 +0200

    adding navbar and home elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
 /* JS */
+/* OPEN AND CLOSING MOBILE NAVBAR */
+const $openNavbar = document.querySelector(".navbar__toggle");
+const $navbarMobile = document.querySelector(".navbar-mobile");
+const $navbarOverlay = document.querySelector(".navbar-mobile__overlay");
+const $closeNavbar = document.querySelector(".navbar-mobile__toggle");
+const $mobileLinks = document.querySelectorAll(".navbar-mobile__links");
+
+function openMobileNav() {
+  $navbarMobile.classList.add("show");
+  $navbarOverlay.classList.add("show");
+}
+
+function closeMobileNav() {
+  $navbarMobile.classList.remove("show");
+  $navbarOverlay.classList.remove("show");
+}
+
+$openNavbar.addEventListener("click", openMobileNav);
+$closeNavbar.addEventListener("click", closeMobileNav);
+$navbarOverlay.addEventListener("click", closeMobileNav);
+$mobileLinks.forEach((navLink) => {
+  navLink.addEventListener("click", closeMobileNav);
+});
