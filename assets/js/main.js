ea81ca3ca52cf488fde3504454a932ca6aabcc8f
Martina Tarantino <martinatarantino02@gmail.com>
2025-10-15 20:45:54 +0200

    added some details

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,15 +5,18 @@ const $navbarMobile = document.querySelector(".navbar-mobile");
 const $navbarOverlay = document.querySelector(".navbar-mobile__overlay");
 const $closeNavbar = document.querySelector(".navbar-mobile__toggle");
 const $mobileLinks = document.querySelectorAll(".navbar-mobile__links");
+const $body = document.querySelector("body");
 
 function openMobileNav() {
   $navbarMobile.classList.add("show");
   $navbarOverlay.classList.add("show");
+  $body.style.overflow = "hidden";
 }
 
 function closeMobileNav() {
   $navbarMobile.classList.remove("show");
   $navbarOverlay.classList.remove("show");
+  $body.style.overflow = "auto";
 }
 
 $openNavbar.addEventListener("click", openMobileNav);
@@ -23,24 +26,41 @@ $mobileLinks.forEach((navLink) => {
   navLink.addEventListener("click", closeMobileNav);
 });
 
+/* MAKING NAVBAR DISAPPEAR AND APPEAR ON SCROLL */
+const $mainNavbar = document.querySelector("header");
+
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", () => {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    $mainNavbar.classList.add("hide");
+  } else {
+    $mainNavbar.classList.remove("hide");
+  }
+
+  lastScrollTop = scrollTop;
+});
+
 /* CREATING PROJECTS ELEMENTS */
 const $projectsContainer = document.querySelector(".projects__container");
 
 /* Array containing all games info */
 const projects = [
   {
-    title: "HorrorTubbies Hotel",
-    img: "../assets/img/tubbies-hotel.jpeg",
-    description:
-      "A short horror game prototype inspired by Outlast, where a distorted Teletubby stalks you through an abandoned hotel. Built in Unreal Engine 5 using Blueprints and custom AI behavior.",
-    tags: ["UE5", "Blueprints", "AI", "Level Design"],
-  },
-  {
     title: "Stardew Valley Prototype",
     img: "../assets/img/starvalley.jpeg",
     description:
       "A gameplay prototype inspired by Stardew Valley, featuring a dynamic inventory and interaction system. Built in Unreal Engine 5 entirely with Blueprints.",
     tags: ["UE5", "Blueprints", "System Design"],
+  },
+  {
+    title: "HorrorTubbies Hotel",
+    img: "../assets/img/tubbies-hotel.jpeg",
+    description:
+      "A short horror game prototype inspired by Outlast, where a distorted Teletubby stalks you through an abandoned hotel. Built in Unreal Engine 5 using Blueprints and custom AI behavior.",
+    tags: ["UE5", "Blueprints", "AI", "Level Design"],
   },
 ];
 
